80f77f234435b31444d9ac72774d55f11aa17f6d
ycxxn <kevin8401128@gmail.com>
2023-03-27 16:23:33 +0800

    init

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import torch
 from mobileformer.mobile_former import mobile_former_26m
 
 model = mobile_former_26m()
+model = model.eval()
 
 ckpt_path = "ckpt/mobile-former-26m.pth"
-ckpt = torch.load(ckpt_path)["state_dict"]
+# ckpt = torch.load(ckpt_path)["state_dict"]
+# model.load_state_dict(ckpt)
 
-model.load_state_dict(ckpt)
+x = torch.randn(1,3,224,224)
+
+y = model(x)
+
+print(y.shape)
